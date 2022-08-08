b61dc8825c0b5e03e00ab3b5652703339c7171f8
Gabriel da Silva Cardoso <cardoso.gabriel@proton.me>
2022-08-08 12:26:58 -0300

    Expands nvim configuration
    * Rewrites vimscript in lua
    
    * Adds treesitter, lsp and git integration to nvim
    
    * Adds missing .gitignore file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,160 @@
+--██╗███╗   ██╗██╗████████╗██╗     ██╗   ██╗ █████╗ 
+--██║████╗  ██║██║╚══██╔══╝██║     ██║   ██║██╔══██╗
+--██║██╔██╗ ██║██║   ██║   ██║     ██║   ██║███████║
+--██║██║╚██╗██║██║   ██║   ██║     ██║   ██║██╔══██║
+--██║██║ ╚████║██║   ██║██╗███████╗╚██████╔╝██║  ██║
+--╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+--==================================================
+
+-- Global Options (vim.o)
+
+-- Local Window Options (vim.wo)
+vim.wo.number = true
+
+-- Local Buffer Optionsn (vim.bo)
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+
+-- Keybindings
+
+-- ctrl+S to save
+local keymap = vim.api.nvim_set_keymap
+keymap('n', '<c-s>', ':w<CR>', {})
+keymap('i', '<c-s>', '<Esc>:w<CR>a', {})
+
+-- split navigation with ctrl + (i/j/k/l)
+local opts = { noremap = true }
+keymap('n', '<c-j>', '<c-w>j', opts)
+keymap('n', '<c-h>', '<c-w>h', opts)
+keymap('n', '<c-k>', '<c-w>k', opts)
+keymap('n', '<c-l>', '<c-w>l', opts)
+
+
+-- Package Manager
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'mhinz/vim-signify'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  use 'junegunn/gv.vim'
+
+end)
+
+-- Plugins
+
+-- git
+--keymap('n', '<leader>gj',  
+-- tree-sitter
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = "all", -- Only use parsers that are maintained
+  highlight = {
+    enable = true, 
+  },
+  indent = {
+  enable = true,
+  }
+}
+
+-- LSP
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+          --workspace = {
+            -- Make the server aware of Neovim runtime files
+            --library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+          --}
+        }
+      }
+    }
+  end
+  server:setup(opts)
+end)
+
+local function nkeymap(key, map)
+  keymap('n', key, map, opts)
+end
+
+nkeymap('gd', ':lua vim.lsp.buf.definition()<cr>')
+nkeymap('gD', ':lua vim.lsp.buf.declaration()<cr>')
+nkeymap('gi', ':lua vim.lsp.buf.implementation()<cr>')
+nkeymap('gw', ':lua vim.lsp.buf.document_symbol()<cr>')
+nkeymap('gw', ':lua vim.lsp.buf.workspace_symbol()<cr>')
+nkeymap('gr', ':lua vim.lsp.buf.references()<cr>')
+nkeymap('gt', ':lua vim.lsp.buf.type_definition()<cr>')
+nkeymap('K', ':lua vim.lsp.buf.hover()<cr>')
+nkeymap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
+nkeymap('<leader>af', ':lua vim.lsp.buf.code_action()<cr>')
+nkeymap('<leader>rn', ':lua vim.lsp.buf.rename()<cr>')
+
+-- completion
+local cmp = require'cmp'
+
+cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+--require('lspconfig')['rust_analyzer'].setup {
+--  capabilities = capabilities
+--}
+
+-- folding
+--vim.opt.foldmethod = "expr"
+--vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
--- .config/nvim/plugins.vim
+++
@@ -1,6 +0,0 @@
-call plug#begin()
- 
-#Plug 'neovim/nvim-lspconfig'
-#Plug 'hrsh7th/nvim-compe'
-
-call plug#end()
